a6f7f6b1eba7a509caeda54a888f0919ea7c5d86
Sergey Yazan <sergey.yazan@gmail.com>
2016-03-18 11:21:22 +0300

    rspec setup

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  Kernel.srand config.seed
+
+  config.order = :random
+
+  config.profile_examples = 5
+
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+end
