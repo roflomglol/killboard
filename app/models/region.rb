db5d09ed5df0496add2bea3f23662a3ec06cc1c6
Sergey Yazan <sergey.yazan@gmail.com>
2016-04-22 12:24:46 +0300

    region has many systems

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,4 +2,6 @@ class Region < ActiveRecord::Base
   validates :name,       presence: true, uniqueness: true
   validates :crest_id,   presence: true, uniqueness: true
   validates :crest_url,  presence: true, uniqueness: true
+
+  has_many :systems
 end
