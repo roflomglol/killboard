9bc77bff62f0b9a7678647ee89dee5ba47ca5cb5
Sergey Yazan <sergey.yazan@gmail.com>
2017-04-18 15:48:40 +0300

    move to api mode

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,7 +1,21 @@
 class System < ActiveRecord::Base
+  # == Constants ============================================================
+
+  # == Attributes ===========================================================
+
+  # == Extensions ===========================================================
+
+  # == Relationships ========================================================
+  belongs_to :region
+  # == Validations ==========================================================
   validates :name,       presence: true, uniqueness: true
   validates :crest_id,   presence: true, uniqueness: true
   validates :crest_url,  presence: true, uniqueness: true
+  # == Scopes ===============================================================
 
-  belongs_to :region
+  # == Callbacks ============================================================
+
+  # == Class Methods ========================================================
+
+  # == Instance Methods =====================================================
 end
