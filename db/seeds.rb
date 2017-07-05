05430f53dbdd43ccf97964ce287a281f08cf93e6
Sergey Yazan <sergey.yazan@gmail.com>
2017-07-05 23:25:42 +0300

    adds pilots to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+names = [
+  'Forsaken Skipper',
+  'Tau AD',
+  'ja-max',
+  'Ownage Activated',
+  'razernc',
+  'Kondorssonsson',
+  'vesage Aucie',
+  'Intigo',
+  'Rei Ayannami',
+  'Romaz Chechen',
+]
+
+names.each do |name|
+  Pilot.find_or_create_by(name: name)
+end
