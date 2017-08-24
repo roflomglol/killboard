b1cc54e45a69e47473cf2b88f0006642f1480e10
Sergey Yazan <sergey.yazan@gmail.com>
2017-08-24 21:29:25 +0300

    adds row_count and from_id params to killmails fetcher

--- lib/killboard/fetchers/corp/killmails_fetcher.rb
+++ lib/killboard/fetchers/corp/killmails_fetcher.rb
@@ -2,6 +2,22 @@ module Fetchers
   module Corp
     class KillmailsFetcher < CorpBaseFetcher
       self.path = '/corp/KillMails.xml.aspx'
+
+      attr_reader :from_id, :row_count
+
+      def initialize(from_id: nil, row_count: 25)
+        @from_id   = from_id
+        @row_count = row_count
+      end
+
+      private
+
+      def params
+        super.merge(
+          fromID: from_id,
+          rowCount: row_count
+        )
+      end
     end
   end
 end
